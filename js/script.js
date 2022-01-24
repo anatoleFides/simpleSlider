ab2b4570e8ceea9ad6092f8683e9ac7ded87c9ca
Anatolii Bedilo <fides2013ml@gmail.com>
2022-01-25 00:14:26 +0300

    create simple slider

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let btnRight = document.querySelector(".btnRight")
+
+let slides = document.querySelectorAll("img")
+
+let i = 0
+
+btnRight.addEventListener("click", function () {
+	i++
+	
+	if (i >= slides.length) {
+
+		slides[i-1].classList.remove("block")
+
+		i = 0
+
+		slides[i].classList.add("block")
+	} else {
+		slides[i-1].classList.remove("block")
+
+		slides[i].classList.add("block")
+	}
+})
